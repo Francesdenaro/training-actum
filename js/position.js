f81814a664a2b8865ffea09ebeadd06c4e76b941
Francesco <fdenaro96@gmail.com>
2018-01-10 09:24:43 +0100

    uploading positioning page and some little changes to other pages

---
+++ js/position.js
@@ -0,0 +1,14 @@
+const BUTTON = document.querySelector('button');
+const IMGS = document.querySelectorAll('img');
+
+BUTTON.addEventListener('click', function() {
+    for(let i = 0; i<IMGS.length; i++) {
+        IMGS[i].classList.toggle('big-img');
+    }
+    if(BUTTON.innerHTML=='big images') {
+        BUTTON.innerHTML = 'small images';
+        console.log('clicked')
+    } else {
+        BUTTON.innerHTML = 'big images';
+    }
+});
